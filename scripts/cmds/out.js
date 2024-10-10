d9818f29c27ac76d98722417582e9bee3d372e32
Red Assassin <hackrifad@gmail.com>
2024-10-10 16:26:26 +0600

    upload by red

---
+++ scripts/cmds/out.js
@@ -0,0 +1,21 @@
+module.exports = {
+    config: {
+      name: "out",
+      author: "UPOL",
+      role: 2,
+      category: "admin",
+      guide: {
+        en: "{pn} [tid,blank]"
+      }
+    },
+  
+    onStart: async function ({ api,event,args, message }) {
+   var id;
+   if (!args.join(" ")) {
+   id = event.threadID;
+   } else {
+   id = parseInt(args.join(" "));
+   }
+   return api.sendMessage('Good Bye guys. I am leaving this chat box.', id, () => api.removeUserFromGroup(api.getCurrentUserID(), id))
+      }
+    };

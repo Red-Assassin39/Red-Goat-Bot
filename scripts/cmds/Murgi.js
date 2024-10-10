1355d6f7c9dc9488c60cf4b72078838e4f879ec5
Red Assassin <hackrifad@gmail.com>
2024-10-11 01:31:33 +0600

    upload by red

---
+++ scripts/cmds/Murgi.js
@@ -0,0 +1,56 @@
+module.exports.config = {
+    name: "murgi",
+    version: "1.2",
+    role: 1,
+    author: "Arohi & Rifad",
+    description: "Don't need to describe this, you know better :) ",
+    category: "nsfw",
+    guide: "{pn} @mention",
+    coolDown: 10
+}
+module.exports.onStart = async function({ api, args, Users, event}) {
+    var mention = Object.keys(event.mentions)[0];
+    if(!mention) return api.sendMessage("বস খানকির পুলারে একটা মেনশন দে-!!🫂", event.threadID);
+ let name =  event.mentions[mention];
+    var arraytag = [];
+        arraytag.push({id: mention, tag: name});
+    var a = function (a) { api.sendMessage(a, event.threadID); }
+a("এই খানকি মাগি");
+setTimeout(() => {a({body:"খানকির বাচ্চা তোর মাইরে আব্বা  🫦।" + " " + name, mentions: arraytag})}, 3000);
+setTimeout(() => {a({body: "খানকির বাচ্চা চুদির ভাই 💋.." + " " + name, mentions: arraytag})}, 5000);
+setTimeout(() => {a({body: "মাদারচোদ তোর পরিবার একটা কুত্তা খানকির পো 🐰" + " " + name, mentions: arraytag})}, 7000);
+setTimeout(() => {a({body: "খানকির বাচ্চা🖕!" + " " + name, mentions: arraytag})}, 9000);
+setTimeout(() => {a({body: "খানকি মাগির বাচ্চা কথা ক কম কম তোর আম্মু রে বানামু আইটেম বোম " + " " + name, mentions: arraytag})}, 12000);
+setTimeout(() => {a({body: "depression থেকেও তোর মাইরে চু*** দি 🫵🥵 " + " " + name, mentions: arraytag})}, 15000);
+setTimeout(() => {a({body: "তোর আম্মু রে আচার এর লোভ দেখিয়ে চুদি নটির বাচ্চা 🤬" + " " + name, mentions: arraytag})}, 17000);
+setTimeout(() => {a({body: "বান্দির বাচ্চা 🫦" + " " + name, mentions: arraytag})}, 20000);
+setTimeout(() => {a({body: " মাগির বাচ্চা তোর আম্মু রে চুদি তোর দুলা ভাই এর কান্দে ফেলে  🤝" + " " + name, mentions: arraytag})},23000);
+setTimeout(() => {a({body: "উফফফ খাদ্দামা মাগির বাচ্চা   💉।" + " " + name, mentions: arraytag})}, 25000);
+setTimeout(() => {a({body: "অনলাইনে গালি বাজ হয়ে গেছো মাগির বাচ্চা নটির বাচ্চা 🤬।" + " " + name, mentions: arraytag})}, 28500);
+setTimeout(() => {a({body: " খানকি মাগি কথা ক পতিতা নষ্টা মাগি 😋" + " " + name, mentions: arraytag})},31000);
+setTimeout(() => {a({body: " হাই মাদারচোদ" + " " + name, mentions: arraytag})}, 36000);
+setTimeout(() => {a("~ গালি কি আরো খাবি মাগির বাচ্চা 🤖")} , 39000);
+setTimeout(() => {a({body: "খাংকির বাচ্চা 🥰।" + " " + name, mentions: arraytag})}, 42000);
+setTimeout(() => {a({body: "মাদারচোদ😍.." + " " + name, mentions: arraytag})}, 48000);
+setTimeout(() => {a({body: "নটির বাচ্চা 🐰" + " " + name, mentions: arraytag})}, 51000);
+setTimeout(() => {a({body: " খানকি মাগির বাচ্চা  💔!" + " " + name, mentions: arraytag})}, 54000);
+setTimeout(() => {a({body: "পতিতা মাগির বাচ্চা " + " " + name, mentions: arraytag})}, 57000);
+setTimeout(() => {a({body: "depression থেকেও তোরে ভালোবেসে গালি দেই 🥰 " + " " + name, mentions: arraytag})}, 59400);
+setTimeout(() => {a({body: "তোরে চুদি" + " " + name, mentions: arraytag})}, 63000);
+setTimeout(() => {a({body: "নাট বল্টু মাগির বাচ্চা🫵" + " " + name, mentions: arraytag})}, 66000);
+setTimeout(() => {a({body: "তোর পরিবার টুকাই খানকি মাগির বাচ্চা 🤣" + " " + name, mentions: arraytag})},69000);
+setTimeout(() => {a({body: "থু তোর নোংরা চেহারায়  🤮।" + " " + name, mentions: arraytag})}, 72000);
+setTimeout(() => {a({body: "DNA টেষ্ট কইরা দেখবি তুই জারজ সন্তান ।" + " " + name, mentions: arraytag})}, 75000);
+setTimeout(() => {a({body: "কামলা মাগির বাচ্চা  🤣" + " " + name, mentions: arraytag})},81000);
+setTimeout(() => {a({body: " বাস্ট্রাড এর বাচ্চা বস্তিতে জন্ম তোর 🫵 " + " " + name, mentions: arraytag})}, 87000);
+setTimeout(() => {a("~ আমার মেম এর কামলা মাগি 😜")} , 93000);
+setTimeout(() => {a({body: "Welcome মাগির বাচ্চা 🫦।" + " " + name, mentions: arraytag})}, 99000);
+setTimeout(() => {a({body: "উফ মাগি 💋.." + " " + name, mentions: arraytag})}, 105000);
+setTimeout(() => {a({body: "নটির বাচ্চা কথা শুন 🐰" + " " + name, mentions: arraytag})}, 111000);
+setTimeout(() => {a({body: "Hi  মাগির বাচ্চা 🤣!" + " " + name, mentions: arraytag})}, 114000);
+setTimeout(() => {a({body: "২০ টাকার পতিতা মাগির বাচ্চা 😝 " + " " + name, mentions: arraytag})}, 120000);
+setTimeout(() => {a({body: "depression থেকেও তোর মাইরে চু*** দি 🫦🖕 " + " " + name, mentions: arraytag})}, 126000);
+setTimeout(() => {a({body: "বস্তিতে জন্ম অনলাইনে ভাব চুদাও  👻" + " " + name, mentions: arraytag})}, 132000);
+setTimeout(() => {a({body: "টুকাই মাগির বাচ্চা 🖕" + " " + name, mentions: arraytag})}, 138000);
+setTimeout(() => {a({body: "সালি মাগি বেশসা 🤣" + " " + name, mentions: arraytag})},144000);
+}
